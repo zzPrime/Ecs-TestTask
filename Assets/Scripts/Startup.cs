831ebcf5876537d3b8163f18afb8275e403c8527
IT <ilyatereshkin@gmail.com>
2022-07-08 16:41:08 +0700

    Startup

---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,69 @@
+using System;
+using Leopotam.EcsLite;
+using Leopotam.EcsLite.UnityEditor;
+using Zenject;
+
+namespace EcsTestProject
+{
+    internal class Startup : IInitializable, ITickable, IFixedTickable, IDisposable
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+        private EcsSystems _fixedUpdSystems;
+
+        public void Initialize()
+        {
+            _world = new EcsWorld();
+            InitializeUpdateSystems();
+            InitializeFixedUpdateSystems();
+        }
+
+        public void Tick()
+        {
+            _systems?.Run();
+        }
+
+        public void FixedTick()
+        {
+            _fixedUpdSystems?.Run();
+        }
+
+        public void Dispose()
+        {
+            if (_systems != null)
+            {
+                _systems.Destroy();
+                _systems = null;
+            }
+
+            if (_fixedUpdSystems != null)
+            {
+                _fixedUpdSystems.Destroy();
+                _fixedUpdSystems = null;
+            }
+
+            _world?.Destroy();
+            _world = null;
+        }
+
+        private void InitializeUpdateSystems()
+        {
+            _systems = new EcsSystems(_world);
+            _systems
+#if UNITY_EDITOR
+                .Add(new EcsWorldDebugSystem())
+#endif
+                .Init();
+        }
+
+        private void InitializeFixedUpdateSystems()
+        {
+            _fixedUpdSystems = new EcsSystems(_world);
+            _fixedUpdSystems
+#if UNITY_EDITOR
+                .Add(new EcsWorldDebugSystem())
+#endif
+                .Init();
+        }
+    }
+}
